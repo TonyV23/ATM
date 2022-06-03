8928a69805b07987e504cd10512933bdc48cfd90
viper23 <htonnycarlos@gmail.com>
2022-06-03 11:56:16 +0200

    define person and customer class

--- src/Actors/Customer.java
+++ src/Actors/Customer.java
@@ -1,4 +1,34 @@
 package Actors;
 
-public class Customer {
+public class Customer extends Person{
+    private static int card_number;
+    private static String user_password;
+
+    // the constructor
+    public Customer(String name, String surname, int card_number, String user_password){
+        super(name, surname);
+        this.card_number = card_number;
+        this.user_password = user_password;
+    }
+
+    // the empty constructor
+    public Customer(){}
+
+    // getters and setters
+
+    public static int getCard_number() {
+        return card_number;
+    }
+
+    public static void setCard_number(int card_number) {
+        Customer.card_number = card_number;
+    }
+
+    public static String getUser_password() {
+        return user_password;
+    }
+
+    public static void setUser_password(String user_password) {
+        Customer.user_password = user_password;
+    }
 }
--- src/Actors/Person.java
+++ src/Actors/Person.java
@@ -1,4 +1,34 @@
 package Actors;
 
 public class Person {
+    private static String name;
+    private static String surname;
+
+    // the constructor
+    public Person(String name, String surname){
+        this.name= name;
+        this.surname = surname;
+    }
+
+    // the empty constructor
+    public Person(){}
+
+
+    // getters and setters
+    public static String getName() {
+        return name;
+    }
+
+    public static void setName(String name) {
+        Person.name = name;
+    }
+
+    public static String getSurname() {
+        return surname;
+    }
+
+    public static void setSurname(String surname) {
+        Person.surname = surname;
+    }
+
 }
